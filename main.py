941cf4cee3c3f47e3f6893e0f9e041508f7f6ef1
bibek190 <bibek9818917036@gmail.com>
2025-06-20 12:25:52 +1000

    Hello python

---
+++ main.py
@@ -0,0 +1,34 @@
+
+
+# print(a+a1)
+# print("The type of a is ",type(a))
+# print("The type of b is ",type(b))
+# print("The type of c is ",type(c))
+
+# list1 = [8,2.3,[-4,5],["apple",'banana']]
+# print(list1)
+
+# tuple1 = (("parrot","sparrow"),("lion","tiger"))
+# print(tuple)
+
+# dict1 = {"name":"Bibek","age":20,"canVote":True}
+# print(dict1)
+# Day 8 ----------------------------------------------
+# a= int(input('Enter first  number '));
+# b= int(input('Enter second  number '));
+
+# print("The value of ",a ,'+',b,"is ",a+b);
+# print("The value of ",a ,'-',b,"is ",a-b);
+# print("The value of ",a ,'%',b,"is ",a%b);
+# print("The value of ",a ,'*',b,"is ",a*b);
+
+# Day 9 ---------------------------------------------------------------------------------
+
+# a = '1'
+# b = '2'
+# print(int(a)+int(b))
+
+name = input("Enter your name: ")
+last_name = input("Enter your last name:")
+print("Welcome to my snake world,", name + last_name)
+input("Do you want to go further?")
